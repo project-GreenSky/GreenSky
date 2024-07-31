cd77c934b9210c691605b4b4a356f7e8b192f669
rishi998 <mehtorishi62203@gmail.com>
2024-07-31 18:27:26 +0530

    AQI tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import Home from "./pages/Home";
-
+import AQIComponent from "./components/Aqicomponent";
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <AQIComponent />
+    </>
+  );
 }
