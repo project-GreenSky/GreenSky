c15d7739640c3057dc39d3b7376892209d678bc9
Vinayak Vohra <vn26vhr@gmail.com>
2024-08-01 18:31:28 +0530

    fix: folder name in import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Home from "./pages/Home";
-import CarbonFootprintCalculator from "./Components/CarbonFootPrintCalculator/CarbonFootPrintCalculator";
+import CarbonFootprintCalculator from "./components/CarbonFootPrintCalculator/CarbonFootPrintCalculator";
 import Aqibody from "./components/AQIcomponents/Aqibody";
 
 export default function App() {
