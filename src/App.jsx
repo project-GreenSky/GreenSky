ca1b837d3e9315453de70c34a29981c0499322e9
rishi998 <mehtorishi62203@gmail.com>
2024-08-01 18:11:06 +0530

    Merge branch 'main' of https://github.com/project-GreenSky/GreenSky

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Home from "./pages/Home";
-// import CarbonFootprintCalculator from "./Components/CarbonFootPrintCalculator/CarbonFootPrintCalculator.jsx";
+import CarbonFootprintCalculator from "./components/CarbonFootPrintCalculator/CarbonFootPrintCalculator";
+import AQIComponent from "./components/Aqicomponent";
 import Aqibody from "./components/AQIcomponents/Aqibody";
 
 export default function App() {
   return (
     <>
       <Aqibody />
-      {/* <CarbonFootprintCalculator /> */}
-      {/* <Home /> */}
+      <CarbonFootprintCalculator />
+      <Home />
     </>
   );
 }
