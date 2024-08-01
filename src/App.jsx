1fbdede0294e273dead03bb0e967c3014b0081ee
HardikBhanot <hardikbhanot123@gmail.com>
2024-08-01 11:31:25 +0530

    Added Carbon Footprint Calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
-export default function App() {
-  return <div>Hello World!!</div>;
-}
+import React from 'react';
+import CarbonFootprintCalculator from './Components/CarbonFootPrintCalculator/CarbonFootPrintCalculator.jsx';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Carbon Footprint Calculator</h1>
+      {/* <h2>{process.env.REACT_APP_CARBON_KEY}</h2> */}
+      <CarbonFootprintCalculator />
+    </div>
+  );
+};
+
+export default App;
