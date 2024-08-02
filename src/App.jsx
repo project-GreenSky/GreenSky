8cf5e76b349e5c4d9cbf0c37ac5f80bbf46ee9d4
Sahil Pahuja <19059570004@aryabhattacollege.ac.in>
2024-08-03 01:00:45 +0530

    facts added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import CarbonFootprintCalculator from "./components/CarbonFootPrintCalculator/CarbonFootPrintCalculator";
 import Aqibody from "./components/AQIcomponents/Aqibody";
-
+import Facts from "./components/Facts";
 export default function App() {
   return (
     <>
       <Navbar />
       <Aqibody />
       <CarbonFootprintCalculator />
+      <Facts />
       <Footer />
     </>
   );
