a477961cbc38dd357aa517fb4a45fea45b640bd0
SumitDwivedi24X <sumitdwivedi1100@gmail.com>
2024-08-02 21:01:21 +0530

    Add Blog component to App.jsx & readme update

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import CarbonFootprintCalculator from "./components/CarbonFootPrintCalculator/CarbonFootPrintCalculator";
 import Aqibody from "./components/AQIcomponents/Aqibody";
+import Blog from "./pages/Blog";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Blog />
       <Aqibody />
       <CarbonFootprintCalculator />
       <Footer />
