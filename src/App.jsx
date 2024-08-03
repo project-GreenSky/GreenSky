85a6bb63146263b7b0a6ec52a92488b9e9c283e5
Vinayak Vohra <vn26vhr@gmail.com>
2024-08-03 16:29:31 +0530

    add: page-404

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Blog from "./pages/Blog";
 import Home from "./pages/Home";
 import { useState } from "react";
 
+import notfound from "./assets/not-found.svg";
+
 export default function App() {
   const [page, setPage] = useState("home");
 
@@ -20,6 +22,7 @@ export default function App() {
         <Route path="/about" element={<InProgress />} />
         <Route path="/contact" element={<InProgress />} />
         <Route path="/services" element={<InProgress />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>
@@ -30,6 +33,15 @@ function InProgress() {
   return (
     <div className="h-dvh flex items-center justify-center text-sxl">
       Coming Soon ...
+    </div>
+  );
+}
+
+function PageNotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-dvh gap-5 p-10">
+      <img src={notfound} alt="404" />
+      <p className="text-2xl">PAGE NOT FOUND</p>
     </div>
   );
 }
